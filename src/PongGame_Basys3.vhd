9add5700b7a64056d780428d1e959c81c75abb19
Abhijeet Surakanti <abhijeetsurakanti@gmail.com>
2025-07-13 15:36:25 -0400

    add/configure constraint and wrapper declaration

--- src/PongGame_Basys3.vhd
+++ src/PongGame_Basys3.vhd
@@ -6,47 +6,35 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use work.physical_io_package.ALL;
 
 entity PongGame_Basys3 is
     port (
+		reset: in std_logic;
+		clk: in std_logic;
         btnL: in std_logic;
         btnR: in std_logic;
         seg: out std_logic_vector(6 downto 0);
         an: out std_logic_vector(3 downto 0);
-        led: out std_logic_vector(15 downto 0);
-    );
+        led: out std_logic_vector(15 downto 0)
+	);
 end PongGame_Basys3;
 
 architecture PongGame_Basys3_ARCH of PongGame_Basys3 is
     
-    component SevenSegmentDriver
-		port (
-			reset: in std_logic;
-			clock: in std_logic;
-
-			digit3: in std_logic_vector(3 downto 0);    --leftmost digit
-			digit2: in std_logic_vector(3 downto 0);    --2nd from left digit
-			digit1: in std_logic_vector(3 downto 0);    --3rd from left digit
-			digit0: in std_logic_vector(3 downto 0);    --rightmost digit
-
-			blank3: in std_logic;    --leftmost digit
-			blank2: in std_logic;    --2nd from left digit
-			blank1: in std_logic;    --3rd from left digit
-			blank0: in std_logic;    --rightmost digit
-
-			sevenSegs: out std_logic_vector(6 downto 0);    --MSB=g, LSB=a
-			anodes:    out std_logic_vector(3 downto 0)    --MSB=leftmost digit
-	);
-	end component;
-
     component PongGame is
         port (
-            leftPaddle: in std_logic;
-            rightPaddle: in std_logic;
-            
-        );
-end component;
+			reset: in std_logic;
+			clock: in std_logic;
+			leftPaddle: in std_logic;
+			rightPaddle: in std_logic;
+
+			ledField: out std_logic_vector(15 downto 0);
+			leftScore: out std_logic_vector(6 downto 0);
+			rightScore: out std_logic_vector(6 downto 0)
+		);
+	end component;
+
 begin
 
 
